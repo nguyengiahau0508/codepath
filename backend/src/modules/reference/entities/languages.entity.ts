cc382a711169e4ff8a46db7af8695b57b48f7f74
Nguyen Gia Hau <nguyengiahau2004@gmail.com>
2025-12-24 00:13:40 +0700

    feat: add index to Language entity for improved query performance

--- backend/src/modules/reference/entities/languages.entity.ts
+++ backend/src/modules/reference/entities/languages.entity.ts
@@ -3,9 +3,10 @@
 import { BaseEntity } from '../../../common/base/base.entity';
 import { ProblemChecker } from '../../problem-authoring/entities/problem_checkers.entity';
 import { ProblemStatement } from 'src/modules/problem-authoring/entities/problem_statements.entity';
-import { Entity, Column, OneToMany } from 'typeorm';
+import { Entity, Column, OneToMany, Index } from 'typeorm';
 
 @Entity('languages')
+@Index('idx_languages_code', ['code'])
 export class Language extends BaseEntity {
   @Column({ length: 10, unique: true })
   code: string;
